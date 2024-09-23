2c59f854f7fbed1aa17481211aee9eb5a2889953
Pauline Oliveira <apaulineoliveira@gmail.com>
2024-09-23 01:35:03 -0300

    Fix (resolve bug de transcricao de áudio)

--- src/script.js
+++ src/script.js
@@ -1,21 +1,46 @@
-speak.addEventListener('click',function(){
-    var speech = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-    const RECOGNITION = new SpeechRecognition();
-    RECOGNITION.interimResults = true;
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.continuous = false; // Experimente mudar para true se necessário
+recognition.lang = 'pt-BR'; // Configura para português brasileiro
+let isListening = false;
+let finalTranscript = '';
 
-    RECOGNITION.addEventListener('result', e => {
-        const TRANSCRIPT = Array.from(e.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .join('')
+const button = document.getElementById('speak');
+const textarea = document.getElementById('textarea');
 
-        document.getElementById("textarea").innerHTML = TRANSCRIPT;
-        console.log(TRANSCRIPT);
-    });
-    
-    if (speech == true) {
-        RECOGNITION.start();
+button.addEventListener('click', function() {
+    if (!isListening) {
+        recognition.start();
+        isListening = true;
+        button.textContent = 'Escutando... Clique novamente para parar';
+        console.log('Reconhecimento de fala iniciado');
+    } else {
+        recognition.stop();
+        isListening = false;
+        button.textContent = 'Fale para gerar o texto';
+        textarea.textContent = finalTranscript;  // Exibe o texto final no textarea
+        console.log('Reconhecimento de fala parado');
     }
-})
+});
+
+recognition.addEventListener('result', function(e) {
+    console.log('Evento result capturado', e); // Verifica se o evento result está sendo disparado
+    const interimTranscript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+        
+    finalTranscript = interimTranscript;
+    console.log('Transcrição capturada: ', finalTranscript);  // Verifica o texto capturado
+    textarea.textContent = finalTranscript; // Atualiza o textarea enquanto fala
+});
+
+recognition.addEventListener('end', function() {
+    console.log('Reconhecimento de fala finalizado');
+});
+
+recognition.addEventListener('error', function(event) {
+    console.error('Erro no reconhecimento de fala:', event.error);
+});
